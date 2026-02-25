79c0d2c7d4e092f50b503d6839c9b39c18ecf683
1968-501 <samesamechan0412@gmail.com>
2026-02-25 22:32:16 +0900

    feat, fix: async.js -> 위치수정|| index.html 주석 추가

--- async.js
+++ async.js
@@ -4,15 +4,17 @@ const pokemonName = document.querySelector('h2');
 const pokemonImg = document.querySelector('img');
 const usertype = document.querySelector('.user_pockmon_type'); 
 const weaktype = document.querySelector('.weak_type');
-const resisttype = document.querySelector('.weak_type');
+const resisttype = document.querySelector('.resist_type');
 
 searchBtn.addEventListener('click', async () => {
     const name = input.value.toLowerCase();
 
+    
     try {
 
         // 이름, 이미지 생성
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+            // console.log(reponse.ok)
         if (!response.ok) throw new Error();
 
         const data = await response.json();
@@ -22,12 +24,11 @@ searchBtn.addEventListener('click', async () => {
         // 포켓몬 image 가져오기
         pokemonImg.src = data.sprites.front_default;
 
-        // 상성 가져오기
-        
+        // 포켓몬 타입 가져오기
         const typeName = data.types.map(item => item.type.name);
-        const typeURL = data.types.map(item => item.type.url);
+        usertype.textContent = typeName.join(' ');
 
-        usertype.textContent = typeName.join(', ');
+        // 포켓몬 상성
         
 
     } catch (error) {
